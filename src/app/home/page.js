f56214948d814930c1103409ea66465b77b10b2c
nadeemhh <flow669900@gmail.com>
2025-03-26 14:43:05 +0530

    update

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -5,6 +5,9 @@ export const metadata = {
   title: "coslomart Store - Best Online Shopping",
   description: "Shop the latest products at the best prices. Fast shipping and great discounts!",
   keywords: "ecommerce, online shopping, best deals, fast delivery",
+  icons: {
+    icon: "images/coslomartfavicon.jpg",
+  }
  
 };
 
--- src/app/home/products/page.js
+++ src/app/home/products/page.js
@@ -6,7 +6,9 @@ export const metadata = {
   title: "product page",
   description: "Shop the latest products at the best prices. Fast shipping and great discounts!",
   keywords: "ecommerce, online shopping, best deals, fast delivery",
- 
+  icons: {
+    icon: "/images/coslomartfavicon.jpg",
+  }
 };
 
 export default function Products() {
