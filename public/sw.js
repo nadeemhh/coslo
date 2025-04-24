81b56f16ddf900409c942303ff36d637c82e6d6e
nadeemhh <flow669900@gmail.com>
2025-04-24 21:48:49 +0530

    update

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+// public/sw.js
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('coslomart-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          'images/coslomartfavicon.jpg',
+          '/manifest.json',
+          'https://www.coslomart.com/',
+          'https://www.coslomart.com/home',
+          // List critical CSS/JS assets
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,12 @@ export default function RootLayout({ children }) {
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
     rel="stylesheet"
   />
+
+<link rel="manifest" href="/manifest.json" />
+<meta name="theme-color" content="#1389F0" />
+<link rel="apple-touch-icon" href="images/coslomartfavicon.jpg" />
+<meta name="mobile-web-app-capable" content="yes" />
+
             </head>
       <body>
         {/* <h1>nav1</h1>  */}
