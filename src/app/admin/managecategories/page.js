75cad9f75ce973c28da5c28581d1f7fa4c9c626f
nadeemhh <flow669900@gmail.com>
2025-05-17 14:06:37 +0530

    update

--- src/app/admin/managecategories/page.js
+++ src/app/admin/managecategories/page.js
@@ -9,11 +9,12 @@ import Goback from '../../back.js'
 export default function Page() {
 
   const [activeCategory, setActiveCategory] = useState(null);
+  const [updateCategory, setupdateCategory] = useState(null);
   const [changeurl, setchangeurl] = useState(false);
   const [addcategory, setAddCategory] = useState(false);
   const [subcategory, setsubcategory] = useState(false);
   const [categories, setCategories] = useState([]);
-
+console.log(categories)
  
   const refreshCategories = () => {
     fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/category/`)
@@ -37,7 +38,11 @@ export default function Page() {
   };
 
 
- 
+   const toggleupdate = () => {
+   
+    setupdateCategory(!updateCategory);
+  };
+
 
 
  // stop scrool when active input
@@ -58,7 +63,7 @@ export default function Page() {
      
  <div className="add-category-location" style={{height:'650px',overflowY:'auto'}}>
         <div className="add-category">
-          <h2>Available Categories</h2>
+          <h2 style={{fontSize:'25px'}}>Available Categories</h2>
          
           <div className="category-actions" style={{display:'flex',justifyContent:'flex-start',marginBottom:'30px',gap:'30px'}}>
         
@@ -75,12 +80,13 @@ export default function Page() {
 
           <div className="category-actions">
            
-          <NestedDropdown342 categories={categories} changeurl={setchangeurl} activeCategory={activeCategory} setActiveCategory={setActiveCategory} />
+          <NestedDropdown342 categories={categories} changeurl={setchangeurl} activeCategory={activeCategory} setActiveCategory={setActiveCategory} setupdateCategory={setupdateCategory}/>
 
        {addcategory && <Addcategory  subcategory={subcategory} toggleaddcategory={toggleAddCategory} refreshCategories={refreshCategories} activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>}
 
           </div>
 
+{updateCategory && <Addcategory toggleaddcategory={toggleupdate} refreshCategories={refreshCategories} updateCategory={updateCategory}/>}
         
 
         </div>
@@ -94,7 +100,8 @@ export default function Page() {
 
 
 
-function Addcategory({ toggleaddcategory, refreshCategories ,subcategory,activeCategory, setActiveCategory}) {
+function Addcategory({ toggleaddcategory, refreshCategories ,subcategory=false,activeCategory=false, setActiveCategory=false ,updateCategory=false}) {
+  
   const [categoryName, setCategoryName] = useState('');
   const [categoryDescription, setCategoryDescription] = useState('');
   const [categoryImages, setCategoryImages] = useState([]);
@@ -105,7 +112,7 @@ function Addcategory({ toggleaddcategory, refreshCategories ,subcategory,activeC
     const files = Array.from(event.target.files);
     setCategoryImages(files);
     const imageUrls = files.map((file) => URL.createObjectURL(file));
-    setcategoryimages((prevImages) => [...prevImages, ...imageUrls]);
+    setcategoryimages((prevImages) => [...imageUrls]);
 
   };
 
@@ -122,20 +129,21 @@ function Addcategory({ toggleaddcategory, refreshCategories ,subcategory,activeC
       return;
     
     }
+
+        document.querySelector('.loaderoverlay').style.display='flex';
   
     const formData = new FormData();
+
     formData.append("name", categoryName);
     // formData.append("description", categoryDescription);
     formData.append("categoryImage", categoryImages[0]); // Only sending one file
+
   if(subcategory){
    let id = document.querySelector('.active342').getAttribute('categoryid');
    console.log(id)
     formData.append("parent", id);
   }
-    // Append multiple images
-  // categoryImages.forEach((image, index) => {
-  //   formData.append("categoryImage", image); // Ensure your backend supports multiple images
-  // });
+
   
     try {
       const token = localStorage.getItem('admintoken');
@@ -153,16 +161,78 @@ function Addcategory({ toggleaddcategory, refreshCategories ,subcategory,activeC
         setActiveCategory(null)
         toggleaddcategory();
         refreshCategories(); // Reload category list
+         document.querySelector('.loaderoverlay').style.display='none';
       } else {
         const errorData = await response.json();
         alert("Failed to add category: " + errorData.message);
+         document.querySelector('.loaderoverlay').style.display='none';
       }
     } catch (error) {
       console.error("Error:", error);
       alert("Something went wrong!");
+       document.querySelector('.loaderoverlay').style.display='none';
     }
   };
+
+
+ const handleupdate  = async () => {
+
+
+    if (!categoryName || categoryimages.length === 0) {
+      alert("Please fill all fields and upload an image.");
+
+      return;
+    
+    }
   
+       document.querySelector('.loaderoverlay').style.display='flex';
+
+    const formData = new FormData();
+
+    formData.append("name", categoryName);
+    // formData.append("description", categoryDescription);
+    formData.append("categoryImage", categoryImages[0]); // Only sending one file
+
+
+  
+    try {
+      const token = localStorage.getItem('admintoken');
+
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/category/update/${updateCategory.id}`, {
+        method: "PATCH",
+        headers: {
+          ...(token && { Authorization: `Bearer ${token}` }), // Add token if it exists
+        },
+        body: formData,
+      });
+  
+      if (response.ok) {
+        alert("Category updated successfully!");
+        toggleaddcategory();
+        refreshCategories(); // Reload category list
+         document.querySelector('.loaderoverlay').style.display='none';
+      } else {
+        const errorData = await response.json();
+        alert("Failed to update category: " + errorData.message);
+         document.querySelector('.loaderoverlay').style.display='none';
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Something went wrong!");
+       document.querySelector('.loaderoverlay').style.display='none';
+    }
+  };
+
+  
+
+   useEffect(() => {
+
+if(updateCategory !== false){
+    setcategoryimages([updateCategory.image]);
+    setCategoryName(updateCategory.name)
+}
+
+  }, []);
 
   return (
     <div className="modal-overlay">
@@ -214,15 +284,14 @@ function Addcategory({ toggleaddcategory, refreshCategories ,subcategory,activeC
  ))}
 </div>
 </div>
-
-
-
           <div className="button-group">
             <button className="cancel-button" onClick={toggleaddcategory}>
               Cancel
             </button>
-            <button className="save-button" onClick={handleSubmit}>
-              Add Category
+            <button className="save-button" onClick={()=>{
+              updateCategory === false ? handleSubmit():handleupdate()
+              }}>
+            { updateCategory === false ? <>Add Category</> : <>Update Category</>}
             </button>
           </div>
         </div>
@@ -248,7 +317,7 @@ const generatePaths = (categories, parentPath = '') => {
 };
 
 
-const NestedDropdown342 = ({changeurl,categories,activeCategory, setActiveCategory}) => {
+const NestedDropdown342 = ({changeurl,categories,activeCategory, setActiveCategory,setupdateCategory}) => {
  
   const [openCategories, setOpenCategories] = useState([]);
 //  const [activeCategory, setActiveCategory] = useState(null);
@@ -279,13 +348,16 @@ const NestedDropdown342 = ({changeurl,categories,activeCategory, setActiveCatego
               <div className={`category-item342 ${isActive ? "active342" : ""}`}  categoryid={category.id} >
                 {hasChildren ? (
                   <button className="toggle-icon342" onClick={() => toggleCategory(category.id)}>
-                    {isOpen ? <i className="fas fa-minus" style={{fontSize:'17px',color:'#9b9b9b'}}></i> : <i className="fas fa-plus" style={{fontSize:'17px',color:'#9b9b9b'}}></i>}
+                    {isOpen ? <i className="fas fa-minus" style={{fontSize:'17px',color:'black'}}></i> : <i className="fas fa-plus" style={{fontSize:'17px',color:'black'}}></i>}
                   </button>
                 ) : (
                   <span className="cross-icon342">
-                    <i className="fas fa-plus" style={{ visibility: "hidden" ,fontSize:'17px',color:'#9b9b9b'}}></i>
+                    <i className="fas fa-plus" style={{ visibility: "hidden" ,fontSize:'17px',color:'black'}}></i>
                   </span>
                 )}
+
+                <i className="fas fa-pencil-alt" style={{fontSize:'15px',color:'green'}} onClick={()=>(setupdateCategory({id:category.id,name:category.name,image:category.image}))}></i>
+                
                 <span onClick={() => setActive(category.id)} className="category-name342">
                   {category.name}
                 </span>
