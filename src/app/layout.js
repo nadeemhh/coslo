52024a77896240b3637348a5a8ccf59c7e4db0e9
nadeemhh <flow669900@gmail.com>
2025-06-12 09:34:25 +0530

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-
+import Script from "next/script";
 import { GlobalProvider } from './context/GlobalState';
 
 import "./globals.css";
@@ -7,6 +7,24 @@ export default function RootLayout({ children }) {
     <GlobalProvider>
     <html lang="en">
       <head>
+         {/* Google Tag (gtag.js) */}
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-5WVFJ6PP67"
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-5WVFJ6PP67');
+              `,
+            }}
+          />
+
       <link
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
     rel="stylesheet"
